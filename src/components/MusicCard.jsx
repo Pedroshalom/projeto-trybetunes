9a86a85c46eba0b9a00dea45f23c9f36c6279ecc
Pedro <pedroshalommrn@hotmail.com>
2022-08-31 14:34:10 -0300

    add req 7, 8 e 9

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from '../Loading';
+
+class MusicCard extends React.Component {
+  state = {
+    CarregandoTexto: false,
+    isFavorite: false,
+  };
+
+  componentDidMount() {
+    this.favorit();
+  }
+
+  favorit = () => {
+    const { favList, trackId } = this.props;
+    if (favList.some((song) => song.trackId === trackId)) {
+      this.setState({ isFavorite: true });
+    }
+  };
+
+  adicionaAosFavoritos = async () => {
+    this.setState({ CarregandoTexto: true });
+    await addSong(this.props);
+    this.setState({ CarregandoTexto: false });
+  };
+
+  handleCheckbox = ({ target: { checked } }) => {
+    if (checked === true) {
+      this.setState({ isFavorite: true });
+      this.adicionaAosFavoritos();
+    }
+  };
+
+  render() {
+    const { trackName, previewUrl, trackId } = this.props;
+    const { CarregandoTexto, isFavorite } = this.state;
+    if (CarregandoTexto) return <Carregando />;
+    return (
+      <div className="lista2">
+        <h4>{trackName}</h4>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          Não Encontrado!
+          {' '}
+          <p>audio</p>
+          .
+        </audio>
+        <div>
+          {' '}
+          <label htmlFor="favoriteInput">
+            <input
+              id="favoriteInput"
+              data-testid={ `checkbox-music-${trackId}` }
+              type="checkbox"
+              onChange={ this.handleCheckbox }
+              checked={ isFavorite }
+            />
+            Favorita
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
+  favList: PropTypes.shape({
+    some: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from '../Loading';
 
 class Album extends React.Component {
+  state = {
+    CarregandoTexto: false,
+    listaDeMusicas: [],
+    artistName: '',
+    collectionName: '',
+    artworkUrl100: '',
+    musicaFavorita: [],
+  };
+
+  componentDidMount() {
+    this.getlistaDeMusicas();
+    this.getListOfFavs();
+  }
+
+  updateState = (requestedSongs) => {
+    this.setState({ artistName: requestedSongs[0].artistName,
+      collectionName: requestedSongs[0].collectionName,
+      artworkUrl100: requestedSongs[1].artworkUrl100 });
+  };
+
+  getlistaDeMusicas = async () => {
+    const { match: { params: { id } } } = this.props;
+    const requestedSongs = await getMusics(id);
+    this.setState(
+      { listaDeMusicas: requestedSongs },
+      () => this.updateState(requestedSongs),
+    );
+  };
+
+  getListOfFavs = async () => {
+    this.setState({ CarregandoTexto: true });
+    const muscasFavoritas = await getFavoriteSongs();
+    this.setState({ CarregandoTexto: false,
+      musicaFavorita: muscasFavoritas });
+  };
+
   render() {
+    const { listaDeMusicas, artistName,
+      collectionName, artworkUrl100,
+      CarregandoTexto, musicaFavorita } = this.state;
+    if (CarregandoTexto) return <Carregando />;
     return (
-      <>
+      <div data-testid="page-album">
         <Header />
-        <div data-testid="page-album" />
-      </>
+        <section>
+          <div>
+            <img alt="album cover" src={ artworkUrl100 } />
+            <h3 data-testid="album-name">{`${collectionName}`}</h3>
+            <p data-testid="artist-name">{`By ${artistName}`}</p>
+
+          </div>
+          <div>
+            {
+              listaDeMusicas.flatMap((song) => song.kind === 'song'
+            && <MusicCard { ... song } favList={ musicaFavorita } />)
+            }
+          </div>
+        </section>
+      </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Album;
